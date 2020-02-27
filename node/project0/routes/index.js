468325613eb2a00422e0ab0bac9e50543214dad2
HurJungMin <gjwjdlas@sookmyung.ac.kr>
2020-02-27 22:32:36 +0900

    ..

--- node/project0/routes/index.js
+++ node/project0/routes/index.js
@@ -5,4 +5,10 @@ router.get("/", function(req, res) {
   res.send("서버 배포가 성공했다");
 });
 
+// index.js에서는 url 경로를 설정한다.
+// ** 이 경로를 제대로 설정해주지 않으면 에러가 뜬다.
+
+router.use("blogs", require("./blogs"));
+router.use("user", require("./user"));
+
 module.exports = router;
