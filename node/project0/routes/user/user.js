baa8b7229a4404c758c78c2514270f7572362134
HurJungMin <gjwjdlas@sookmyung.ac.kr>
2020-03-06 23:22:10 +0900

    회원가입 crypto 공부

--- node/project0/routes/user/user.js
+++ node/project0/routes/user/user.js
@@ -2,5 +2,34 @@ var express = require("express");
 var router = express.Router();
 
 const { au, sc, rm } = require("../../modules/utils");
+// salt뿌려서 암호화
 const crypto = require("crypto");
+// 로그인 시 토큰 발급
+// const jwt = require("../../modules/jwt");
 
+router.post("/signup", async (req, res) => {
+  const { userId, pwd, email } = req.body;
+
+  //공백 값 체크
+  if (!userId || !pwd || !email) {
+    res.json({
+      code: sc.BAD_REQUEST,
+      json: au.successFalse(rm.NULL_VALUE)
+    });
+  }
+
+  try {
+    //pwd salt값 뿌려주기
+    const salt = crypto.randomBytes(32).toString("base64");
+    console.log(salt);
+    const derivedKey = crypto.pbkdf2Sync(password, salt, 1, 32, "sha512");
+    const key = derivedKey.toString("base64");
+    console.log(key);
+  } catch (err) {
+    console.log(err);
+    res.json({
+      code: sc.INTERNAL_SERVER_ERROR,
+      json: au.successFalse(rm.INTERNAL_SERVER_ERROR)
+    });
+  }
+});
